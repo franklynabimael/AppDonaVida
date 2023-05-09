14954aff97687ae8d8fe133a0effe12626281dda
Franklin Hernandez <franklynabimael@gmail.com>
2023-05-08 20:43:41 -0400

    Final, presentacion

--- AppDonaVida/Config/CenterDonorConfig.cs
+++ AppDonaVida/Config/CenterDonorConfig.cs
@@ -8,6 +8,8 @@ public class CenterDonorConfig : IEntityTypeConfiguration<CenterDonor>
 {
     private const string adminId = "B22698B8-42A2-4115-9631-1C2D1E2AC5F7";
     private const string centerId = "cruzroja";
+
+
     public void Configure(EntityTypeBuilder<CenterDonor> builder)
     {
         builder.HasKey(x => x.Id);
